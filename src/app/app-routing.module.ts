3f38edecd481b498ad65bb1825e39ca0aab1991e
Abhiram Jayakumar <abhiramjayakumar8@gmail.com>
2022-11-15 12:59:22 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegisterComponent },
+  { path: 'view', component: ViewDetailsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
